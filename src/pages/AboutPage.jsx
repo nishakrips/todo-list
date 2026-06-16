0a904f7cddaa230b518b62fa5ac65355c0e285e9
Nisha <kripalaninisha@gmail.com>
2026-06-16 16:19:16 -0400

    added readme and scrshots

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -3,7 +3,10 @@ function AboutPage() {
     <div>
       <h1>About Todo App</h1>
       <p>
-        This is a simple todo list application built with React and Spring Boot.
+        This is a simple todo list application built with React.The app allows
+        users to manage their tasks by adding, editing, completing, and deleting
+        todo items. It also includes features for filtering and sorting tasks,
+        as well as user authentication for a personalized experience.
       </p>
       <h2>Features</h2>
       <ul>
@@ -11,12 +14,23 @@ function AboutPage() {
         <li>Mark todo items as complete</li>
         <li>Edit existing todo items</li>
         <li>Delete todo items</li>
+        <li>Filter todo items by status (active, completed, all)</li>
+        <li>Sort todo items by title or creation date</li>
+        <li>User authentication and protected routes</li>
+        <li>Search todo items</li>
       </ul>
       <h2>Technologies Used</h2>
       <ul>
         <li>React</li>
         <li>React Router</li>
         <li>Vite</li>
+        <li>CSS Modules</li>
+        <li>
+          React Hooks useState, useEffect and Reducers for state management
+        </li>
+        <li>UseMemo for performance optimization with Sort and Filter</li>
+        <li>Fetch API for backend communication</li>
+        <li>Deployed on Vercel</li>
       </ul>
     </div>
   )
